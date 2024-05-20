27de0cd66460c9835b1b66f299bda46990d1172f
Caputommy <caputommy@gmail.com>
2024-05-20 21:36:50 +0200

    Reworked DecGraph.py

--- multilevel_graphs/DecGraph.py
+++ multilevel_graphs/DecGraph.py
@@ -5,10 +5,16 @@ from multilevel_graphs import Supernode
 class DecGraph:
 
     def __init__(self):
-        self.digraph = nx.DiGraph()
+        self._digraph = nx.DiGraph()
+        self.V = set()
+        self.E = set()
 
-    def add_node(self, supernode_for_adding: Supernode):
-        attr = supernode_for_adding.attr
-        attr['dec_graph'] = supernode_for_adding.dec_graph
-        attr['supernode'] = supernode_for_adding.supernode
-        self.digraph.add_node(supernode_for_adding.key, attr)
+    def add_node(self, node_for_adding: Supernode):
+        self.digraph.add_node(node_for_adding.key)
+        self.V.add(node_for_adding)
+
+'''
+    def add_edge(self, edge_for_adding: Superedge):
+        self.digraph.add_edge(edge_for_adding.tail.key, edge_for_adding.head.key)
+        self.E.add(edge_for_adding)
+'''
--- multilevel_graphs/Supernode.py
+++ multilevel_graphs/Supernode.py
@@ -8,7 +8,14 @@ class Supernode:
         self.key = key
         self.dec_graph = dec_graph
         self.supernode = supernode
-        self.attr = attr
+        for k, v in attr.items():
+            setattr(self, k, v)
 
     def __eq__(self, other):
         return self.key == other.key
+
+    def __hash__(self):
+        return hash(self.key)
+
+    def __str__(self):
+        return str(self.key)
